51872281961217d76bd42ad42c579b01a089caa0
Robert Jones <Robert Jones>
2017-02-10 12:23:52 -0500

    initial commit

---
+++ first.ps1
@@ -0,0 +1,15 @@
+﻿#Read in the csv file
+$machines = Import-Csv .\first.csv
+
+#Set up the log file information and initialize counters
+
+$date = get-date -format MMddyyHHmmss
+$logfile = ".\firstlog"+$date+".log"
+
+#Loop through each record from the file and process the information
+
+For ($i=1; $i -le $machines.count; $i++)
+  {
+    $output="ip address; "+$machines[$i-1].ip
+    write-host $output 
+    }
